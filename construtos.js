da99924311f744fb3459e2ed008951641708ddd9
Kaue Sedrez <kaue.sedrez@gmail.com>
2019-06-29 02:33:11 -0300

    tutorial  completo

--- construtos.js
+++ construtos.js
@@ -1,8 +1,8 @@
 const Componentes =
 `
 <div class="winnetou">
-	<div class="card text-white bg-danger mb-3" id="[[componenteTexto1]]">
-		Voce pode fazer o que voce quiser a partir de agora {{ texto }}
+	<div class="card text-white bg-primary mb-3" id="[[componenteTexto1]]">
+		{{ texto }}
 	</div>
 </div>;
 `;
--- motor.js
+++ motor.js
@@ -1,13 +1,19 @@
 $(function() {
 	var W = new Winnetou();
 
-	W.init();
-
-	W.create('componenteTexto1', 'body', {
-		texto: 'Foda-se o react e sua chatice!!!',
+    W.init();
+    
+    W.create('componenteTexto1', 'body', {
+		texto: 'Dentro do construtos.html escrever todo os componentes envoltos da div winnetou',
 	});
 
 	W.create('componenteTexto1', 'body', {
-		texto: 'Eu sou foda!!',
+		texto: 'Rodar node wcli.js para transformar o construtos.html em .js',
+    });
+    
+    W.create('componenteTexto1', 'body', {
+		texto: 'Ao alterar o construtos.html rodar o wcli novamente para atualizar o js',
 	});
+
+	
 });
