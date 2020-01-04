6bba390f37ad8bfd36edbb08b7b1320852f61699
Kaue Pavillion LINUX <kaue.sedrez@gmail.com>
2020-01-04 16:43:02 -0300

    teste

---
+++ commit.sh
@@ -0,0 +1,19 @@
+clear
+echo "Commit automatico com controle de versao"
+
+echo "Entre com o codigo da versao: "
+read cod
+echo "Entre com o nome da versao: "
+read com
+echo "$cod-$com"
+del version.json
+echo {"version":"$cod"} >> version.json
+
+git add .
+git commit -m "$cod-$com"
+git push
+
+
+echo "version.json gravado com sucesso."
+echo "commit e push realizados com sucesso."
+echo "Production Version: %cod%"
