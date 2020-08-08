275714d37bf0f67b2f8888e615986f6ad5d58a65
Kaue Pavillion LINUX <kaue.sedrez@gmail.com>
2020-08-08 18:43:11 -0300

    t

--- build.js
+++ build.js
@@ -6,8 +6,12 @@
  */
 
 const fs = require("fs");
+const path = require("path");
 
-fs.copyFile("./scaffolding/package.json", "../../", err => {
+const p1 = path.join(__dirname, "/scaffolding/package.json");
+const dest = path.join(__dirname, "../../");
+
+fs.copyFile(p1, dest, err => {
   if (err) throw err;
   console.log("package.json was copied to root");
 });
