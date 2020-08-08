255e02df087e6c3c014c754833186a31b102716e
Kaue Pavillion LINUX <kaue.sedrez@gmail.com>
2020-08-08 19:37:16 -0300

    t

--- build.js
+++ build.js
@@ -11,19 +11,21 @@ const path = require("path");
 const p1 = path.join(__dirname, "/scaffolding/package.json");
 const p2 = path.join(__dirname, "/scaffolding/wbr.js");
 const p3 = path.join(__dirname, "/scaffolding/win.config.js");
-const dest = path.join(__dirname, "../../package.json");
+const dest1 = path.join(__dirname, "../../package.json");
+const dest2 = path.join(__dirname, "../../wbr.js");
+const dest3 = path.join(__dirname, "../../win.config.js");
 
-fs.copyFile(p1, dest, err => {
+fs.copyFile(p1, dest1, err => {
   if (err) throw err;
   console.log("package.json was copied to root");
 });
 
-fs.copyFile(p2, dest, err => {
+fs.copyFile(p2, dest2, err => {
   if (err) throw err;
   console.log("wbr.js was copied to root");
 });
 
-fs.copyFile(p3, dest, err => {
+fs.copyFile(p3, dest3, err => {
   if (err) throw err;
   console.log("win.config.js was copied to root");
 });
