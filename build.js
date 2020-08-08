071b67ee15774791b5f4112209dfe006f423f83f
Kaue Pavillion LINUX <kaue.sedrez@gmail.com>
2020-08-08 18:51:03 -0300

    t

--- build.js
+++ build.js
@@ -9,7 +9,7 @@ const fs = require("fs");
 const path = require("path");
 
 const p1 = path.join(__dirname, "/scaffolding/package.json");
-const dest = path.join(__dirname, "../../");
+const dest = path.join(__dirname, "../../package.json");
 
 fs.copyFile(p1, dest, err => {
   if (err) throw err;
