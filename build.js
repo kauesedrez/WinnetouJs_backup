9523d6e17e7323a24b79255b9b07a162613238d6
Kaue Pavillion LINUX <kaue.sedrez@gmail.com>
2020-08-08 18:38:47 -0300

    teste

--- build.js
+++ build.js
@@ -1,1 +1,13 @@
-console.log("ola mundo");
+/**
+ * Todo:
+ * Saudação profissional ao instalar o winnetoujs
+ * com dicas sobre tutoriais e próximos passos
+ *
+ */
+
+const fs = require("fs");
+
+fs.copyFile("./scaffolding/package.json", "../../", err => {
+  if (err) throw err;
+  console.log("package.json was copied to root");
+});
